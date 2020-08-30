9778d01c43c5703bb1b7f15959910571e3da6a63
craighillelson <craighillelson@gmail.com>
2020-08-30 08:15:00 -0400

    update score_tracker.py
    streamlined the way delta_from_target is calculated

--- score_tracker.py
+++ score_tracker.py
@@ -8,9 +8,10 @@ from functions import (DATES_SCORES_CSV,
                        get_todays_date_string,
                        import_scores,
                        merge_dictionaries,
+                       output_result,
                        print_high_or_low_scores,
-                       # print_score_and_delta,
                        prompt_user_for_score,
+                       prompt_user_for_target,
                        update_user,
                        write_scores_to_csv)
 
@@ -22,23 +23,23 @@ if PREVIOUS_SCORES:
 else:
     print("\nno previous scores")
 
+target_score = prompt_user_for_target()
 todays_score = prompt_user_for_score()
 SCORE_TODAY = {}
 SCORE_TODAY[TODAY_STR] = float(todays_score)
 SCORES_MERGED = merge_dictionaries(SCORE_TODAY, PREVIOUS_SCORES)
+delta_from_target = calculate_delta(todays_score, target_score)
 
 print(f"\ndate: {TODAY_STR}")
+print(f"\ntarget: {target_score}")
 print(f"current score: {todays_score}")
+output_result("delta from target:", delta_from_target)
 
 if len(SCORES_MERGED) > 1:
     most_recent_score = score_values[0]
     print(f"most recent score: {most_recent_score}")
-    delta = round(calculate_delta(todays_score, most_recent_score), 2)
-    print("delta:", end=" ")
-    if delta > 0:
-        print(f"+{delta}")
-    else:
-        print(delta)
+    delta_from_previous_score = calculate_delta(todays_score, most_recent_score)
+    output_result("delta from previous score:", delta_from_previous_score)
     MERGED_VALUES = convert_values_to_list(SCORES_MERGED)
     print(f"average: {round(average(MERGED_VALUES), 2)}")
 else:
